5017b6439cf0db8145b596e964b085e1e0387ab1
kiembully <54204618+kiembully@users.noreply.github.com>
2022-01-09 16:44:37 +0800

    Update connectors

--- pages/nft.js
+++ pages/nft.js
@@ -1,5 +1,5 @@
 import { useWeb3React } from "@web3-react/core"
-import { Injected } from "../components/wallet/Connectors"
+import { Injected } from "../components/connectors"
 import nftStyles from '../styles/nft.module.scss'
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
